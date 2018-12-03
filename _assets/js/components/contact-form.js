75737fc9a9a9534934cf2133941ecae07002a1b9
Oleh Pratsko <olehpratsko@gmail.com>
2018-12-03 10:17:31 +0200

    Update images

--- _assets/js/components/contact-form.js
+++ _assets/js/components/contact-form.js
@@ -4,24 +4,21 @@
   $(document).ready(function () {
     $('#contactForm').on('submit', function(e) {
       e.preventDefault();
-      var firstName = $('#name').val(); //get the first name field value
-      var lastName = $('#lastName').val(); //get the last name field value
+      var name = $('#name').val(); //get the first name field value
       var email = $('#email').val(); //get the name field value
-
-      var name = firstName + " " + lastName;
 
       //send to formspree
       $.ajax({
-        url:'https://formspree.io/xqlqylgm',
-        method:'POST',
-        data:{
-          name:name,
-          _replyto:email,
-          email:email,
-          _subject:'Otter Networks form submission'
+        url: 'https://formspree.io/xqlqylgm',
+        method: 'POST',
+        data: {
+          name: name,
+          _replyto: email,
+          email: email,
+          _subject: 'Otter Networks form submission'
         },
-        dataType:"json",
-        success:function() {
+        dataType: "json",
+        success: function() {
           console.log('success');
           $('#formBlock').hide();
           $('#thankyouBlock').show();
